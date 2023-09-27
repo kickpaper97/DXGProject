20147b724f7b5924d91e1051d198de0fe16e6331
김지섭 <kickpaper97@gmail.com>
2023-09-27 19:24:45 +0900

    수정

--- DirectX_Game/DX_Project/PaperPlease_Contents/BasicActor.cpp
+++ DirectX_Game/DX_Project/PaperPlease_Contents/BasicActor.cpp
@@ -1,16 +1,21 @@
+
 #include "PreCompile.h"
 #include "BasicActor.h"
-
-
-
 
 
 BasicActor::BasicActor()
 {
 }
+
 BasicActor::~BasicActor()
 {
 }
+
+
 void BasicActor::Start()
 {
+	SpriteRenderer = CreateComponent<GameEngineSpriteRenderer>(GetOrder());
+	SpriteRenderer->SetAutoScaleRatio(2.0f);
 }
+
+
