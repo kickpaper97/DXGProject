c50ae75243a7dcd65621d0c3cd2da38be6f7d76f
김지섭 <kickpaper97@gmail.com>
2023-10-12 17:33:45 +0900

    블렌더 기본 코드 적용

--- DirectX_Game/DX_Project/PaperPlease_Contents/ContentsCore.cpp
+++ DirectX_Game/DX_Project/PaperPlease_Contents/ContentsCore.cpp
@@ -2,6 +2,8 @@
 #include "ContentsCore.h"
 
 #include <GameEngineCore/GameEngineSpriteRenderer.h>
+#include <GameEngineCore/GameEngineSampler.h>
+#include <GameEngineCore/GameEngineBlend.h>
 
 #include "TitleLevel.h"
 #include "PlayLevel.h"
@@ -17,15 +19,45 @@ ContentsCore::~ContentsCore()
 {
 }
 
+void ContentsCore::UserRes()
+{
+	{
+		D3D11_BLEND_DESC Desc = {};
+		Desc.IndependentBlendEnable = false;
+		Desc.RenderTarget[0].BlendEnable = true;
+		Desc.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
+
+		Desc.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;
+		Desc.RenderTarget[0].SrcBlend = D3D11_BLEND_ONE; // src팩터
+		Desc.RenderTarget[0].DestBlend = D3D11_BLEND_ONE;
+
+		Desc.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;
+		Desc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_ZERO;
+		Desc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ONE;
+
+		std::shared_ptr<GameEngineBlend> Blend = GameEngineBlend::Create("OverRay", Desc);
+	}
+
+	{
+		std::shared_ptr<GameEngineMaterial> Mat = GameEngineMaterial::Create("2DTextureOver");
+		Mat->SetVertexShader("TextureShader_VS");
+		Mat->SetPixelShader("TextureShader_PS");
+		Mat->SetBlendState("OverRay");
+	}
+}
+
 void ContentsCore::Start()
 {
 
 	GameEngineRenderTarget::IsDepth = false;
 	GameEngineCore::MainWindow.CursorOff();
 
+	UserRes();
+
 	GameEngineCore::CreateLevel<TitleLevel>("TitleLevel");
 	GameEngineCore::CreateLevel<DayOffLevel>("DayOffLevel");
 	GameEngineCore::CreateLevel<PlayLevel>("PlayLevel");
+
 	GameEngineCore::ChangeLevel("TitleLevel");
 
 	// 자기 텍스처 로드해야 한다.
--- DirectX_Game/DX_Project/PaperPlease_Contents/ContentsCore.h
+++ DirectX_Game/DX_Project/PaperPlease_Contents/ContentsCore.h
@@ -35,6 +35,7 @@ protected:
 	void Start() override;
 	void Update(float _Delta) override;
 	void Release() override;
+	void UserRes();
 
 
 private:
